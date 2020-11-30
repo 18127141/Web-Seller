72ff29eb31784d846201017cc5e2b6dac707f340
Trinh Thanh Long <ttlgame123@gmail.com>
2020-12-01 00:02:40 +0700

    Add link

--- JS/admin.js
+++ JS/admin.js
@@ -1,11 +1,10 @@
 // quan li san pham
-list = document.getElementById("List_product")
-add_btn = document.getElementById("Add")
-
-function Add_new_product_GUI(){
-    const item = `
+const list = document.getElementById("List_product");
+add_btn = document.getElementById("Add");
+function Add_new_product_GUI() {
+  const item = `
     <li class = "item"> 
-    Enter img URL <input type = "text" id = "Add_img" placeholder="URL"> <br>
+    <div id ="img_container"><label>Enter URL</label> <input type = "file" id = "Add_img" accept="image/*"></div> <br>
     Enter name <input type = "text" id = "Add_name" placeholder="Name"> <br>
     Enter info <input type = "text" id = "Add_info" placeholder="Info"> <br>
     Enter stock <input type = "text" id = "Add_stock" placeholder="stock"> <br>
@@ -14,15 +13,44 @@ function Add_new_product_GUI(){
     <button id="Add_new_product" type="button" onclick=Add_new_product()> Confrim Add</button>
     <button id="Cancel_new_product" type="button" onclick=cancel_add_product()> Cancel</button>
     </li>
-    `
-    add_btn.disabled = true
-    list.insertAdjacentHTML("beforeend", item)
-    
-}
-function cancel_add_product(){
-    add_btn.disabled = false
-    list.removeChild(list.firstElementChild)
-}
-function Add_new_product(){
+    `;
+  add_btn.disabled = true;
 
+  list.insertAdjacentHTML("beforeend", item);
+  let img_button = document.getElementById("Add_img");
+  img_button.addEventListener("change", function (e) {
+    const reader = new FileReader();
+    reader.onload = function () {
+      const img = new Image();
+      img.src = reader.result;
+      img.width = 200;
+      img.height = 200;
+      console.log(img)
+      const temp = document.getElementById("img_container");
+      while (temp.hasChildNodes()) {
+        temp.removeChild(temp.firstChild);
+      }
+      temp.appendChild(img);
+      temp.insertAdjacentHTML("beforeend",
+        `<button id="Cancel_img" type="button" onclick = cancel_img()>Cancel</button>`);
+      
+    };
+    reader.readAsDataURL(img_button.files[0]);
+  }, false);
 }
+function cancel_img() {
+  const temp = document.getElementById("img_container");
+  while (temp.hasChildNodes()) {
+    temp.removeChild(temp.firstChild);
+  }
+  const item = `
+    <li class = "item"> 
+    <div id ="img_container"><label>Enter URL</label> <input type = "file" id = "Add_img" accept="image/*"></div> <br>`
+  temp.insertAdjacentHTML("beforeend", item)
+  
+}
+function cancel_add_product() {
+  add_btn.disabled = false;
+  list.removeChild(list.firstElementChild);
+}
+function Add_new_product() { }
