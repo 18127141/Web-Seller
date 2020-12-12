4ab9beb247f24dc778148a1ba2b1e7fb046beff6
Trần Phi Long <72856973+18127141@users.noreply.github.com>
2020-12-12 23:40:41 +0700

    Complete Demo Front-end User
    +/User/profile
    +/User/changepass
    +/User/check-order
    +/User/voucher

--- Routes/User.js
+++ Routes/User.js
@@ -9,10 +9,22 @@ router.get('/Register',function(req,res){
 })
 router.get('/',function(req,res)
 {
-    res.render('Login',{layout:'UserProfile'})
+    res.render('user-profile',{layout:'UserProfile'})
 })
 router.get('/profile',function(req,res)
 {
     res.render('user-profile',{layout:'UserProfile'})
+})
+router.get('/changepass',function(req,res)
+{
+    res.render('user-changepass',{layout:'UserProfile'})
+})
+router.get('/check-order',function(req,res)
+{
+    res.render('user-checkorder',{layout:'UserProfile'})
+})
+router.get('/voucher',function(req,res)
+{
+    res.render('user-voucher',{layout:'UserProfile'})
 })
 module.exports = router
